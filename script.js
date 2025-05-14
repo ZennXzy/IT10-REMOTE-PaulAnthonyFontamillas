63b792af57ece496bcd9b56b1d604ad26b75f470
ZennXzy <paulfontamillas7@gmail.com>
2025-05-14 14:01:24 +0800

    Stored button click counts in local Storage

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ var count1 = 0;
 var count2 = 0;
 var countTotal = 0;
 
+
 function btn1Click(){
     count1++;
     countTotal++;
-    document.getElementById('count1').innerHTML = 'Count on Button 1: ' + count1;
+    localStorage.setItem("counter1", cnt1);
+    document.getElementById('count1').innerHTML = 'Counts on Button 1: ' + count1;
     document.getElementById('totalCount').innerHTML = 'Total Counts: ' + countTotal;
 }
 function btn2Click(){
     count2++;
     countTotal++;
-    document.getElementById('count2').innerHTML = 'Count on Button 2: ' + count2;
+    document.getElementById('count2').innerHTML = 'Counts on Button 2: ' + count2;
     document.getElementById('totalCount').innerHTML = 'Total Counts: ' + countTotal;
+    
 }
